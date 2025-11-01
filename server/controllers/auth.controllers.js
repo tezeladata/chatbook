3882f698fc035bdc7366b9acd7eb95a0acce6875
tezeladata <David.tezela@gmail.com>
2025-11-02 02:44:45 +0400

    Day 190 version

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import { catchAsync } from "../utils/catchAsync.js"
 import { User } from "../models/user.model.js"
 import { AppError } from "../utils/appError.js";
+import jwt from "jsonwebtoken"
 
 // Signup
 export const signUp = catchAsync(async (req, res, next) => {
@@ -25,6 +26,20 @@ export const login = catchAsync(async (req, res, next) => {
     if (!isCorrect) return next(new AppError("Incorrect email or password", 400));
 
     user.password = undefined
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES
+    })
+
+    res.cookie('token', token, {
+        maxAge: process.env.COOKIE_EXPIRES*24*60*60*1000,
+        secure: process.env.NODE_ENV === "development" ? false : true,
+        httpOnly: true,
+        sameSite: "Lax"
+    })
 
     res.status(200).json({
         data: {
