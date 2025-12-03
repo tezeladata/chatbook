d52e06051ad4a6438602dd128aae834d09ab04d1
tezeladata <David.tezela@gmail.com>
2025-12-03 23:03:30 +0400

    Day 200 back to mailtrap, gmail did not work for email sending logic

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,9 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
