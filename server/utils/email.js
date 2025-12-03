e4cecaf2cf3e2fdb77e4dcccf8fdf853531ba1e5
tezeladata <David.tezela@gmail.com>
2025-12-03 22:49:43 +0400

    Day 200 gmail sending logic improved

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
